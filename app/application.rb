c8a86fd56194455400b0e7e3936f1c91e603ba1d
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-27 22:09:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,13 +4,24 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-        if req.path=="/items/#{Item}"
-      resp.write "#{@price}"
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+ 
+      search_term = req.params["q"]
+ 
+      if @@items.include?(search_term)
+        resp.write "#{search_term} is one of our items"
+      else
+        resp.write "Couldn't find #{search_term}"
+      end
+ 
     else
-      resp.write "Route not found"
-      resp.status = 404
+      resp.write "Path Not Found"
     end
-    
+ 
     resp.finish
   end
 end
