7608ea00df7a82c7537c9ed53cfe086742c35c5b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-27 22:23:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-        if req.path.match(/items/)
+     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
