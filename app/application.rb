b48435c077ca2981e98b83c4ef97e1056267fda2
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-27 22:33:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
     req = Rack::Request.new(env)
     
      if req.path.match(/items/)
+       song_title = req.path.split("/items/").last
       @@items.each do |item|
         resp.write "#{item}\n"
       end
