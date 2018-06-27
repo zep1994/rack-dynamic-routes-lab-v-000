04a586e06bab2a3bb5c5c4384808a119ff5517e3
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-27 22:37:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
      if req.path.match(/items/)
        song_title = req.path.split("/items/").last
-       if item = @@items.find { |i| i.name =
+       if item_name = @@items.find { |i| i.name == item_name}
       end
       
     
