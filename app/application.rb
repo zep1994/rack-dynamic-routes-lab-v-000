78271d6b2856d752606d46500688de200ae619c7
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-27 22:01:22 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,21 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+        if req.path=="/items/"
+      resp.write "#{@price}"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    
+    if req.path !="/items/"
+      resp.write "Item not found"
+      resp.status = 400
+    end
+ 
+    resp.finish
+  end
+end
