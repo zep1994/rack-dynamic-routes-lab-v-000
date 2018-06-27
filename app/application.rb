db25a5cb873bd25c6e3a2282463b2a83238f546c
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-06-27 22:22:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    
+        if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
     else
       resp.write "Route not found"
       resp.status = 404
